507e5e5163fe389f1d9c5bc04b47801296a2a5ce
asm <andres.a.trevino@protonmail.com>
2024-12-09 19:52:31 -0600

    first commit

---
+++ run.js
@@ -0,0 +1,13 @@
+function double(x) {
+    return x* 2
+}
+
+function triple(x) {
+    return x * 3
+}
+
+export function run(operation, x) {
+    console.log(operation)
+    console.log(x)
+    //Code here
+}
